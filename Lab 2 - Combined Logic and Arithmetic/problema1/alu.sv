7fd71447791cbe652f089f1efaec523922bd3806
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-02-28 23:05:34 -0600

    [ADD] developing adder

---
+++ Lab 2 - Combined Logic and Arithmetic/problema1/alu.sv
@@ -0,0 +1,9 @@
+module alu #(parameter N = 4)
+				(
+				input logic[N-1:0] a, b, 
+				output logic[N-1:0] result 
+				);
+
+				
+
+endmodule
---
+++ Lab 2 - Combined Logic and Arithmetic/problema1/sum_tb.sv
@@ -0,0 +1,36 @@
+module sum_tb();
+
+	logic [3:0] a_tb, b_tb, s_tb;
+	logic cin_tb, cout_tb;
+	
+	// instantiate devide under test
+	sum dut(a_tb, b_tb, cin_tb, s_tb, cout_tb);
+	
+	//apply inputs one at a time
+	
+	initial begin
+	a_tb = 4'b1000;
+	b_tb = 4'b1000;
+	cin_tb = 0;
+	#40ns;
+	a_tb = 4'b0000;
+	b_tb = 4'b0000;
+	cin_tb = 0;
+	#40ns;
+	a_tb = 4'b1001;
+	b_tb = 4'b1001;
+	cin_tb = 0;
+	#40ns;
+	a_tb = 4'b1111;
+	b_tb = 4'b1111;
+	cin_tb = 0;
+	#40ns;
+	a_tb = 4'b1001;
+	b_tb = 4'b1111;
+	cin_tb = 0;
+	#40ns;
+	
+	end
+
+
+endmodule
