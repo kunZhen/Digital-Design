5e98614e425b21ca3ae12a6c82f78c30b8600b89
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-02-29 22:38:46 -0600

    [UPDATE] alu inputs and added more test cases in alu_tb

--- Lab 2 - Combined Logic and Arithmetic/problema1/alu.sv
+++ Lab 2 - Combined Logic and Arithmetic/problema1/alu.sv
@@ -1,6 +1,8 @@
 module alu #(parameter N = 4)
 				(
 				input logic[N-1:0] a, b, 
+				input logic [2:0] op,
+				input logic op_sum, op_subt,
 				output logic[N-1:0] result,
 				output logic carrying
 				);
--- Lab 2 - Combined Logic and Arithmetic/problema1/alu_tb.sv
+++ Lab 2 - Combined Logic and Arithmetic/problema1/alu_tb.sv
@@ -1,25 +1,84 @@
 module alu_tb #(parameter N = 4) ();
 
 	logic [N-1:0] a_tb, b_tb, result_tb;
-	logic carrying_tb;
+	logic [2:0] op_tb;
+	logic op_sum_tb, op_subt_tb, carrying_tb;
 	
 	// instantiate devide under test
-	alu #(N) dut(a_tb, b_tb, result_tb, carrying_tb);
+	alu #(N) dut(a_tb, 
+					b_tb, 
+					op_tb, 
+					op_sum_tb, 
+					op_subt_tb, 
+					result_tb, 
+					carrying_tb);
 	
-	//apply inputs one at a time
+	// apply inputs one at a time
+	// low logic level = pressed
+	// high logic level = not pressed
 	
 	initial begin
 	a_tb = 4'b1000;
 	b_tb = 4'b1000;
+	op_tb = 3'b111; // 0 : 000
+	op_sum_tb = 0;
+	op_subt_tb = 0;
 	#10ns;
 	a_tb = 4'b1001;
-	b_tb = 4'b1000;
+	b_tb = 4'b1001;
+	op_tb = 3'b110; // 1 : 001
+	op_sum_tb = 0;
+	op_subt_tb = 0;
+	#10ns;
+	a_tb = 4'b1010;
+	b_tb = 4'b1010;
+	op_tb = 3'b101; // 2 : 010
+	op_sum_tb = 0;
+	op_subt_tb = 0;
 	#10ns;
 	a_tb = 4'b1011;
-	b_tb = 4'b1000;
+	b_tb = 4'b1011;
+	op_tb = 3'b100; // 3 : 011
+	op_sum_tb = 0;
+	op_subt_tb = 0;
 	#10ns;
+	a_tb = 4'b1000;
+	b_tb = 4'b1000;
+	op_tb = 3'b011; // 4 : 100
+	op_sum_tb = 0;
+	op_subt_tb = 0;
+	#10ns;
+	a_tb = 4'b1000;
+	b_tb = 4'b1000;
+	op_tb = 3'b010; // 5 : 101
+	op_sum_tb = 0;
+	op_subt_tb = 0;
+	#10ns;
+	a_tb = 4'b1000;
+	b_tb = 4'b1000;
+	op_tb = 3'b001; // 6 : 110
+	op_sum_tb = 0;
+	op_subt_tb = 0;
+	#10ns;
+	a_tb = 4'b1000;
+	b_tb = 4'b1000;
+	op_tb = 3'b000; // 7 : 111
+	op_sum_tb = 0;
+	op_subt_tb = 0;
+	#10ns;
+	
+	// To add or substract 
 	a_tb = 4'b1111;
 	b_tb = 4'b1000;
+	op_tb = 3'b111; 
+	op_sum_tb = 1; // sum
+	op_subt_tb = 0;
+	#10ns;
+	a_tb = 4'b1111;
+	b_tb = 4'b1111;
+	op_tb = 3'b111;
+	op_sum_tb = 0; 
+	op_subt_tb = 1; // substract
 	#10ns;
 	
 	end
