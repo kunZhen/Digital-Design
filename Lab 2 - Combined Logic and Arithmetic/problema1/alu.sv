9dc004df1b41c963e05980667944ee8c32f5b1ab
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-03-05 21:07:41 -0600

    [ADD] module and instantiated in alu

--- Lab 2 - Combined Logic and Arithmetic/problema1/alu.sv
+++ Lab 2 - Combined Logic and Arithmetic/problema1/alu.sv
@@ -3,7 +3,7 @@ module alu #(parameter N = 4)
 				input logic[N-1:0] a, b, 
 				input logic [2:0] op,
 				input logic op_sum, op_subt,
-				output logic[N-1:0] result, sumResult, subResult, diviResult,
+				output logic[N-1:0] result, sumResult, subResult, diviResult, moduResult,
 				output logic [(N*2)-1:0] multiResult,
 				output logic carryingSum, carryingSubt
 				);
@@ -13,16 +13,18 @@ module alu #(parameter N = 4)
 	
 	assign sumSubt = op_sum ^ op_subt; // check to see if addition or substraction is at high level 
 	
+	//instantiating arithmetic operations
 	sum #(N) adder(a, b, 0, sumResult, carryingSum);
 	subt #(N) subtract(a, b, 1, subResult, carryingSubt);
-	BitwiseMultiplier #(N) multiplier(a, b, multiResult);
 	divid #(N) divi(a, b, diviResult);
+	modu #(N) mod(a, b, moduResult);
+	BitwiseMultiplier #(N) multiplier(a, b, multiResult);
 	
 	
 	// multiplexer
 	assign result = sumSubt ? (op_sum ? sumResult : subResult)
 									: ( op[2] 	? ( op[1] 	? ( op[0] 	? (4'b1110)   		// 000
-																					: (4'b0111) )		// 001
+																					: (diviResult) )		// 001
 																	: ( op[0] 	? (4'b1100) 		// 010
 																					: (4'b0011) ) )   // 011
 													: ( op[1] 	? ( op[0] 	? (4'b1100) 		// 100
@@ -30,9 +32,7 @@ module alu #(parameter N = 4)
 																	: ( op[0] 	? (4'b1100) 		// 110
 																					: (4'b0011) ) ) );// 111
 	
-	// instantiate
-	
-	//sum #(N) adder(a, b, 0, result, carrying);
+
 				
 
 endmodule
