f0ba1f123b2b29e0e2a962201546e07b7d3580ba
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-04-16 02:38:59 -0600

    [ADD] comments in Battleship

--- Lab 3 - Sequential Logic and Controllers/VgaController/Battleship.sv
+++ Lab 3 - Sequential Logic and Controllers/VgaController/Battleship.sv
@@ -7,19 +7,21 @@ module Battleship (
 	output logic hsync, vsync,
 	output logic sync_b, blank_b,
 	output logic [7:0] r, g, b,
-	output logic[6:0] ships_placed_seg,
+	
+	// siete segmentos para mostrar el número de barcos colocados y la cantidad de barcos restantes.
+	output logic[6:0] ships_placed_seg, 
 	output logic[6:0] amount_of_ships_seg
 	);
 	
 	
-	reg clk_ms;
+	reg clk_ms; // Registro para dividir la frecuencia del reloj clk
 	
-	reg [2:0] i_actual, j_actual;
-	reg [2:0] i_next, j_next;
+	reg [2:0] i_actual, j_actual; // Coordenadas actuales del jugador en el tablero
+	reg [2:0] i_next, j_next; // Coordenadas siguientes del jugador en el tablero
 	
-	reg [2:0] pc_ships = 3'b001;
+	reg [2:0] pc_ships = 3'b001; // Cantidad de barcos de la PC
 	
-	reg [2:0] player_ships = 3'b001; // hacer input en switches
+	reg [2:0] player_ships = 3'b001; //  Cantidad de barcos del jugador
 	
 	
 	logic player_turn, pc_turn, placing_ships, is_victory, is_defeat, finished_placing;
@@ -28,11 +30,13 @@ module Battleship (
 	reg[2:0] ships_placed;
 							  
 							  
+	// Divide la frecuencia del reloj clk para generar una señal de reloj para el monitor VGA
 	vga_clock clkdiv (
 		clk, clk_ms
 	);
 	
-
+	
+	// Controla el proceso de colocación de barcos por parte del jugador
 	place_ship place_ship(
 		.player_place_ship(player_place_ship), // switch player selected cell
 		.placing_ships(placing_ships), // state
@@ -42,7 +46,8 @@ module Battleship (
 		.finished_placing(finished_placing) // how many ships have been placed
 	);
 	
-						
+	
+	// Máquina de estados finitos que controla el flujo del juego
 	FSM fsm(
 	  .clk(clk),
 	  .rst(rst),
@@ -61,7 +66,7 @@ module Battleship (
 	  .is_defeat(is_defeat)
 	);
 	
-	
+	// Controla el movimiento del jugador en el tablero
 	controls movement_controls(
 		.i_actual(i_actual), .j_actual(j_actual), .ships_placed(ships_placed), .amount_of_ships(amount_of_ships),
 		.move_up(move_up), .move_down(move_down), .move_left(move_left), .move_right(move_right),
@@ -69,22 +74,21 @@ module Battleship (
 		.i_next(i_next), .j_next(j_next)
 	);
 	
+	// Actualiza las coordenadas del jugador en el tablero
 	updateIndex updateIJ(
 		i_next, j_next, clk_ms, rst, player_turn, placing_ships,
 		i_actual, j_actual
 	);
 	
-	
+	// Genera señales de video VGA para mostrar el tablero del juego en un monitor
 	vga vga(
 		clk, i_actual, j_actual,
 		vgaclk, hsync, vsync, sync_b, blank_b, r, g, b
 	);
 	
-	
+	// Decodifica las señales de barcos colocados y cantidad de barcos restantes para visualización en siete segmentos
 	decoder ships_placed_deco(ships_placed, ships_placed_seg);
-	
 	decoder amount_of_ships_deco(amount_of_ships, amount_of_ships_seg);
-	
 	
 	
 endmodule
