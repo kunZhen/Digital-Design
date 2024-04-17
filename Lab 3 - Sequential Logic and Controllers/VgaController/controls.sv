75837bfc10c5dce76985d61c3a84210ccd0e2aec
Jose Andres Vargas Torres <tamiguisimo@gmail.com>
2024-04-17 17:08:54 -0600

    [ADD] change some things about battleship functionality

--- Lab 3 - Sequential Logic and Controllers/VgaController/controls.sv
+++ Lab 3 - Sequential Logic and Controllers/VgaController/controls.sv
@@ -28,31 +28,28 @@ module controls(
 	parameter MAX_INDEX = 4;
 				
 	always @(negedge clk or negedge rst) begin
+    // Señal reset activa, reestablece las coordenadas
+    if (!rst) begin
+        i_next <= 0;
+        j_next <= 0;
+    end else begin
+        // Movimientos verticales
+        if(!move_up && i_actual > 0) begin
+            i_next <= i_actual - 1;
+        end
+        if(!move_down && i_actual < MAX_INDEX) begin
+            i_next <= i_actual + 1;
+        end
+        // Movimientos horizontales
+        if(!move_right && j_actual < MAX_INDEX) begin
+            j_next <= j_actual + 1;
+        end
+        if(!move_left && j_actual > 0) begin
+            j_next <= j_actual - 1;
+        end
+    end
+end
+
 	
-		// Señal reset activa, reestablece las coordenadas
-		if (!rst) begin
-			i_next = 0;
-			j_next = 0;
-			
-		end else begin
-		
-			// If the position is not allowed
-			if(i_actual == MAX_INDEX - (amount_of_ships - ships_placed - 1) && move_down == 0) i_next = i_actual;
-			else if(i_actual == 3'b000 && move_up == 0) i_next = i_actual;
-			else if(j_actual == MAX_INDEX && move_right == 0) j_next = j_actual;
-			else if(j_actual == 3'b000 && move_left == 0) j_next = j_actual;
-			else begin
-			
-				if(!move_up) i_next = i_actual - 1;
-				if(!move_down) i_next = i_actual + 1;
-				if(!move_right) j_next = j_actual + 1;
-				if(!move_left) j_next = j_actual - 1;
-			end
-			
-		end
-		
-	end 
-	
-	//assign can_move = (!move_up || !move_right || !move_left || !move_down);
 					 
 endmodule
